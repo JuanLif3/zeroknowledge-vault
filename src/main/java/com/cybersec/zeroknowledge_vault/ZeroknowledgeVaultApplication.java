8fb1eca25376c63ae814f17ac6d4ca7e02d20a3e
JuanLif3 <juanitololilifeloli@gmail.com>
2026-04-08 15:37:06 -0400

    chore: initial spring boot project setup

---
+++ src/main/java/com/cybersec/zeroknowledge_vault/ZeroknowledgeVaultApplication.java
@@ -0,0 +1,13 @@
+package com.cybersec.zeroknowledge_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroknowledgeVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroknowledgeVaultApplication.class, args);
+	}
+
+}
